265cde2e8417dd18b83c708d469f47e02218b3b8
FadyTarekYohana <fadytarekyohana@gmail.com>
2021-09-26 00:13:13 +0200

    add profiles

--- src/main/java/com/mycompany/tasklistapp/NewMain.java
+++ src/main/java/com/mycompany/tasklistapp/NewMain.java
@@ -17,145 +17,233 @@ import java.util.UUID;
 public class NewMain {
 
     public static void main(String[] args) throws IOException {
-        File f = new File("tasks.json");
+        String username, password;
+        Scanner scanner = new Scanner(System.in);
+        int x = 0;
+
+        File f = new File("users.json");
         if (!f.exists()) {
             try {
-                File file = new File("tasks.json");
+                File file = new File("users.json");
                 file.createNewFile();
             } catch (Exception e) {
             }
         }
-
         Gson gson = new Gson();
-        Reader reader = Files.newBufferedReader(Paths.get("tasks.json"));
-        Map<String, Object> json = gson.fromJson(reader, Map.class);
+        Reader reader = Files.newBufferedReader(Paths.get("users.json"));
+        Map<String, String> usersJson = gson.fromJson(reader, Map.class);
         reader.close();
-        Map<String, Object> items = new HashMap<>();
+        Map<String, String> loginInfo = new HashMap<>();
         try {
-            items.putAll((Map<String, Object>) json);
+            loginInfo.putAll((Map<String, String>) usersJson);
         } catch (Exception e) {
         }
 
-        ArrayList<String> uniqueID = new ArrayList<>();
-        int i = 0, choice = 0;
-        String id, title, desc;
-        boolean done;
-        Scanner scanner = new Scanner(System.in);
-
-        LOOP:
+        OUTER:
         while (true) {
-            System.out.print("Please choose one of the following options:\n1- Add a new task\n2- Delete a task\n3- Edit a task\n4- List all tasks\n5- Exit\n");
             while (true) {
+                System.out.println("Welcome to the task list app!\n1- Sign in\n2- Sign up\n3- Exit");
                 try {
-                    choice = scanner.nextInt();
-                    System.out.println();
+                    x = scanner.nextInt();
                     break;
                 } catch (InputMismatchException e) {
-                    System.out.println("Please enter a number from one of the options (1-5)!");
-                    System.out.println();
-                    System.out.print("Please choose one of the following options:\n1- Add a new task\n2- Delete a task\n3- Edit a task\n4- List all tasks\n5- Exit\n");
+                    System.out.println("Please enter a number from one of the options (1-3)!");
                     scanner.nextLine();
+                    System.out.println();
                 }
             }
-            switch (choice) {
+            switch (x) {
                 case 1:
-                    System.out.println("Please enter the title of the new task or -1 to go back:");
-                    scanner.nextLine();
-                    title = scanner.nextLine();
-                    if (title.equals("-1")) {
+                    System.out.println("Please enter your username or -1 to go back:");
+                    username = scanner.next();
+                    if (username.equals("-1")) {
                         System.out.println();
                         continue;
                     }
-                    System.out.println("Please enter the description of the new task:");
-                    desc = scanner.nextLine();
-                    while (true) {
-                        try {
-                            System.out.println("Is the task done? Please enter true or false:");
-                            done = scanner.nextBoolean();
-                            break;
-
-                        } catch (InputMismatchException e) {
-                            System.out.println("Please enter true or false!");
-                            scanner.nextLine();
-                        }
+                    scanner.nextLine();
+                    System.out.println("Please enter your password:");
+                    password = scanner.nextLine();
+                    if (!loginInfo.containsKey(username) || !loginInfo.containsValue(password)) {
+                        System.out.println("Username or password is incorrect!");
+                        continue;
                     }
-                    uniqueID.add(UUID.randomUUID().toString());
-                    items.put(uniqueID.get(i), new Item(title, desc, done));
-                    i++;
-                    System.out.println();
                     break;
                 case 2:
-                    System.out.println("Please enter the ID of the task you wish to delete or -1 to go back:");
-                    scanner.nextLine();
-                    id = scanner.nextLine();
-                    if (id.equals("-1")) {
+                    System.out.println("Please enter your username or -1 to go back:");
+                    username = scanner.next();
+                    if (username.equals("-1")) {
+                        System.out.println();
+                        continue;
+                    }else if (loginInfo.containsKey(username)) {
+                        System.out.println("Username is not available.");
                         continue;
                     }
-                    if (items.containsKey(id)) {
-                        items.remove(id);
-                        System.out.println("Task deleted.");
-                        System.out.println();
-                    } else {
-                        System.out.println("There is no task with that ID.");
-                        System.out.println();
-                    }
-
+                    System.out.println("Please enter your password:");
+                    password = scanner.next();
+                    loginInfo.put(username, password);
                     break;
                 case 3:
-                    System.out.println("Please enter the ID of the task you wish to edit or -1 to go back:");
-                    scanner.nextLine();
-                    id = scanner.nextLine();
-                    if (id.equals("-1")) {
-                        continue;
+                    break OUTER;
+                default:
+                    System.out.println("Please enter a number from one of the options (1-3)!");
+                    System.out.println();
+                    continue;
+            }
+
+            if (!loginInfo.isEmpty()) {
+                try {
+                    Writer writer = Files.newBufferedWriter(Paths.get("users.json"));
+                    gson.toJson(loginInfo, writer);
+                    writer.close();
+                } catch (Exception ex) {
+                }
+            } else {
+                try {
+                    Writer writer = Files.newBufferedWriter(Paths.get("users.json"));
+                    writer.write("");
+                    writer.close();
+                } catch (Exception ex) {
+                }
+            }
+
+            File f2 = new File(username + "tasks.json");
+            if (!f2.exists()) {
+                try {
+                    File file2 = new File(username + "tasks.json");
+                    file2.createNewFile();
+                } catch (Exception e) {
+                }
+            }
+            gson = new Gson();
+            reader = Files.newBufferedReader(Paths.get(username + "tasks.json"));
+            Map<String, Object> tasksJson = gson.fromJson(reader, Map.class);
+            reader.close();
+            Map<String, Object> items = new HashMap<>();
+            try {
+                items.putAll((Map<String, Object>) tasksJson);
+            } catch (Exception e) {
+            }
+
+            ArrayList<String> uniqueID = new ArrayList<>();
+            int i = 0, choice = 0;
+            String id, title, desc;
+            boolean done;
+
+            INNER:
+            while (true) {
+                System.out.print("Please choose one of the following options:\n1- Add a new task\n2- Delete a task\n3- Edit a task\n4- List all tasks\n5- Sign out\n");
+                while (true) {
+                    try {
+                        choice = scanner.nextInt();
+                        System.out.println();
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.out.println("Please enter a number from one of the options (1-5)!");
+                        System.out.println();
+                        System.out.print("Please choose one of the following options:\n1- Add a new task\n2- Delete a task\n3- Edit a task\n4- List all tasks\n5- Sign out\n");
+                        scanner.nextLine();
                     }
-                    if (items.containsKey(id)) {
-                        System.out.println("Please enter the title of the task:");
+                }
+                switch (choice) {
+                    case 1:
+                        System.out.println("Please enter the title of the new task or -1 to go back:");
+                        scanner.nextLine();
                         title = scanner.nextLine();
-                        System.out.println("Please enter the description of the task:");
+                        if (title.equals("-1")) {
+                            System.out.println();
+                            continue;
+                        }
+                        System.out.println("Please enter the description of the new task:");
                         desc = scanner.nextLine();
-                        System.out.println("Is the task done? Please enter true or false:");
-                        done = scanner.nextBoolean();
-                        items.replace(id, new Item(title, desc, done));
+                        while (true) {
+                            try {
+                                System.out.println("Is the task done? Please enter true or false:");
+                                done = scanner.nextBoolean();
+                                break;
+
+                            } catch (InputMismatchException e) {
+                                System.out.println("Please enter true or false!");
+                                scanner.nextLine();
+                            }
+                        }
+                        uniqueID.add(UUID.randomUUID().toString());
+                        items.put(uniqueID.get(i), new Item(title, desc, done));
+                        i++;
                         System.out.println();
-                    } else {
-                        System.out.println("There is no task with that ID.");
-                        System.out.println();
-                    }
-                    break;
-                case 4:
-                    if (items.isEmpty()) {
-                        System.out.println("There are no tasks.");
-                        System.out.println();
-                    } else {
-                        for (Object task : items.keySet()) {
-                            String key = (String) task;
-                            String value = items.get(task).toString();
-                            System.out.println("ID: " + key + "\n" + value);
+                        break;
+                    case 2:
+                        System.out.println("Please enter the ID of the task you wish to delete or -1 to go back:");
+                        scanner.nextLine();
+                        id = scanner.nextLine();
+                        if (id.equals("-1")) {
+                            continue;
+                        }
+                        if (items.containsKey(id)) {
+                            items.remove(id);
+                            System.out.println("Task deleted.");
+                            System.out.println();
+                        } else {
+                            System.out.println("There is no task with that ID.");
                             System.out.println();
                         }
-                    }
-                    break;
-                case 5:
-                    break LOOP;
-                default:
-                    System.out.println("Please enter a number from one of the options (1-5)!");
-                    System.out.println();
-                    break;
+
+                        break;
+                    case 3:
+                        System.out.println("Please enter the ID of the task you wish to edit or -1 to go back:");
+                        scanner.nextLine();
+                        id = scanner.nextLine();
+                        if (id.equals("-1")) {
+                            continue;
+                        }
+                        if (items.containsKey(id)) {
+                            System.out.println("Please enter the title of the task:");
+                            title = scanner.nextLine();
+                            System.out.println("Please enter the description of the task:");
+                            desc = scanner.nextLine();
+                            System.out.println("Is the task done? Please enter true or false:");
+                            done = scanner.nextBoolean();
+                            items.replace(id, new Item(title, desc, done));
+                            System.out.println();
+                        } else {
+                            System.out.println("There is no task with that ID.");
+                            System.out.println();
+                        }
+                        break;
+                    case 4:
+                        if (items.isEmpty()) {
+                            System.out.println("There are no tasks.");
+                            System.out.println();
+                        } else {
+                            for (Object task : items.keySet()) {
+                                String key = (String) task;
+                                String value = items.get(task).toString();
+                                System.out.println("ID: " + key + "\n" + value);
+                                System.out.println();
+                            }
+                        }
+                        break;
+                    case 5:
+                        break INNER;
+                    default:
+                        System.out.println("Please enter a number from one of the options (1-5)!");
+                        System.out.println();
+                        break;
+                }
             }
-        }
-        if (!items.isEmpty()) {
-            try {
-                Writer writer = Files.newBufferedWriter(Paths.get("tasks.json"));
-                gson.toJson(items, writer);
-                writer.close();
-            } catch (Exception ex) {
-            }
-        } else {
-            try {
-                Writer writer = Files.newBufferedWriter(Paths.get("tasks.json"));
-                writer.write("");
-                writer.close();
-            } catch (Exception ex) {
+            if (!items.isEmpty()) {
+                try {
+                    Writer writer = Files.newBufferedWriter(Paths.get(username + "tasks.json"));
+                    gson.toJson(items, writer);
+                    writer.close();
+                } catch (Exception ex) {
+                }
+            } else {
+                try {
+                    Writer writer = Files.newBufferedWriter(Paths.get(username + "tasks.json"));
+                    writer.write("");
+                    writer.close();
+                } catch (Exception ex) {
+                }
             }
         }
     }
